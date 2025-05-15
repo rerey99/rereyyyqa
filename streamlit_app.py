f6d945a4769c75afde8dac9818885951f4121f26
rerey99 <28.reyqaaliifahkhansa.xg@gmail.com>
2025-05-15 14:52:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 kue manis")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    " kue manis pemanis hidup [docs.streamlit.io](https://docs.streamlit.io/)."
 )
