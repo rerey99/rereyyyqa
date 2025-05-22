be682bbc408f99d43c8710eebe3fdceb183688e4
rerey99 <28.reyqaaliifahkhansa.xg@gmail.com>
2025-05-22 14:49:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ st.write(
 st.image("IMG-20250514-WA0036.jpg")
 
 
-#st.header ("Aplikasi mengecrk nilai genap/ganjil")
-angka = st.number_input("Tulis sebuah angka:", value=0, steps=1)
+st.header ("Aplikasi mengecrk nilai genap/ganjil")
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
 
 if (angka % 2) == 0:
     st.write(f"{angka} adalah bilangan genap") 
